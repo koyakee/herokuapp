7339110fed1f4f1203b47d991b33723856eb3225
小泉陽太郎 <koyake@koizumiyoutarounoMacBook-Air.local>
2025-03-25 14:19:00 +0900

    ログイン/ログアウト機能実装のため、deviseを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_24_093651) do
+ActiveRecord::Schema.define(version: 2025_03_25_034358) do
 
   create_table "reservations", force: :cascade do |t|
     t.datetime "checkInDate"
@@ -37,6 +37,11 @@ ActiveRecord::Schema.define(version: 2025_03_24_093651) do
     t.string "introduce"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
